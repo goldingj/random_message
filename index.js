e22963fe1f13821d8dd4579823ca87bffc9fcca0
goldingj <47582726+goldingj@users.noreply.github.com>
2021-08-27 19:52:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+let messageOptions = [" I like eggs", "I slapped her in the face with a piece of pizza", "Hide yo kids, Hide yo wife", "I like turtles", "I was eating a hamburger and it took it, I don't know where it's at"];
 
+
+
+function message (){
+
+    let random = messageOptions[Math.floor(messageOptions.length*Math.random())];
+    return random;
+
+}
+
+console.log(message());
